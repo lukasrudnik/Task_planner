94e93d45e0c72dba2d5d22158a11ff50381c3675
Lukasz Rudnik <lukas.rudnik@gmail.com>
2017-08-09 17:53:56 +0200

    CRUD

---
+++ TaskPlanner/src/TaskPlannerBundle/Form/TaskType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TaskPlannerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('priority')->add('done')->add('deadline')->add('user')->add('category')->add('comments');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TaskPlannerBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'taskplannerbundle_task';
+    }
+
+
+}
